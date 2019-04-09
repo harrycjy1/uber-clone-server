ae15aba64e1bf6daab5e2ac21a519d9027558407
choddol <harrycjy123@gmail.com>
2019-04-09 16:56:16 +0900

    1-20

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import app from "./app";
+import dotenv from "dotenv";
+dotenv.config();
 import { Options } from "graphql-yoga";
 import { createConnection } from "typeorm";
 import connectionOptions from "./ormConfig";
--- src/types/graph.d.ts
+++ src/types/graph.d.ts
@@ -1,8 +1,11 @@
-export const typeDefs = ["type HelloResponse {\n  text: String!\n  error: Boolean!\n}\n\ntype Query {\n  sayHello(name: String!): HelloResponse!\n}\n"];
+export const typeDefs = [
+  "type HelloResponse {\n  text: String!\n  error: Boolean!\n}\n\ntype Query {\n  sayHello(name: String!): HelloResponse!\n  user: User\n}\n\ntype Place {\n  id: Int!\n  name: String!\n  lat: Float!\n  lng: Float!\n  address: String!\n  isFav: Boolean!\n  createdAt: String!\n  updatedAt: String\n}\n\ntype User {\n  id: Int!\n  email: String!\n  verifiedEmail: Boolean!\n  firstName: String!\n  lastName: String!\n  age: Int!\n  password: String\n  phoneNumber: String\n  verifiedPhoneNumber: Boolean!\n  profilePhoto: String\n  createdAt: String!\n  updatedAt: String\n  fullName: String\n  isDriving: Boolean!\n  isRiding: Boolean!\n  isTaken: Boolean!\n  lastLng: Float\n  lastLat: Float\n  lastOrientation: Float\n}\n\ntype Verification {\n  id: String!\n  target: String!\n  payload: String!\n  key: String!\n  used: Boolean!\n  createdAt: String!\n  updatedAt: String!\n}\n"
+];
 /* tslint:disable */
 
 export interface Query {
   sayHello: HelloResponse;
+  user: User | null;
 }
 
 export interface SayHelloQueryArgs {
@@ -12,4 +15,47 @@ export interface SayHelloQueryArgs {
 export interface HelloResponse {
   text: string;
   error: boolean;
+}
+
+export interface User {
+  id: number;
+  email: string;
+  verifiedEmail: boolean;
+  firstName: string;
+  lastName: string;
+  age: number;
+  password: string | null;
+  phoneNumber: string | null;
+  verifiedPhoneNumber: boolean;
+  profilePhoto: string | null;
+  createdAt: string;
+  updatedAt: string | null;
+  fullName: string | null;
+  isDriving: boolean;
+  isRiding: boolean;
+  isTaken: boolean;
+  lastLng: number | null;
+  lastLat: number | null;
+  lastOrientation: number | null;
+}
+
+export interface Place {
+  id: number;
+  name: string;
+  lat: number;
+  lng: number;
+  address: string;
+  isFav: boolean;
+  createdAt: string;
+  updatedAt: string | null;
+}
+
+export interface Verification {
+  id: string;
+  target: string;
+  payload: string;
+  key: string;
+  used: boolean;
+  createdAt: string;
+  updatedAt: string;
 }
