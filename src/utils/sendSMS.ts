21e1e22c04081d60b1042b3a2837b8b32233c521
Choddol <choddol@jojin-yeongs-MacBook-Pro.local>
2019-04-13 19:22:26 +0900

    shhhhhhhhhhit

--- src/utils/sendSMS.ts
+++ src/utils/sendSMS.ts
@@ -1,13 +1,9 @@
-// import Twilio from "twilio";
+import Twilio from "twilio";
 
-const TWILIO_ACCOUNT_SID = "AC7cdc74b135411a9840739d9107fe7715";
-const TWILIO_TOKEN = "17d66a6ccdab53d60252df5c6b823290";
-const TwilioClient = require("twilio")(TWILIO_ACCOUNT_SID, TWILIO_TOKEN);
-
-// const twilioClient = Twilio(
-//   process.env.TWILIO_ACCOUNT_SID,
-//   process.env.TWILIO_TOKEN
-// );
+const TwilioClient = Twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_TOKEN
+);
 
 export const sendSMS = (to: string, body: string) => {
   return TwilioClient.messages.create({
