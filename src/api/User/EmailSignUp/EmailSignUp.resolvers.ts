442c9b4ae5040919c29a4225068d910fe29f8687
Choddol <choddol@jojin-yeongs-MacBook-Pro.local>
2019-04-15 22:10:28 +0900

    done #1.40

---
+++ src/api/User/EmailSignUp/EmailSignUp.resolvers.ts
@@ -0,0 +1,42 @@
+import { Resolvers } from "../../../resolvers";
+import {
+  EmailSignUpMutationArgs,
+  EmailSignUpResponse
+} from "../../../types/graph";
+import User from "../../../entities/User";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    EmailSignUp: async (
+      _,
+      args: EmailSignUpMutationArgs
+    ): Promise<EmailSignUpResponse> => {
+      const { email } = args;
+      try {
+        //email로 이미 가입된 유저가 있는지 체크
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+          return {
+            ok: false,
+            error: "u already signed up!",
+            token: null
+          };
+        }
+        const newUser = await User.create({ ...args }).save();
+        return {
+          ok: true,
+          error: null,
+          token: "coming soon"
+        };
+      } catch (error) {
+        return {
+          ok: false,
+          error: error.message,
+          token: null
+        };
+      }
+    }
+  }
+};
+
+export default resolvers;
