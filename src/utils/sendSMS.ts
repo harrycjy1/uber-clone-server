677bc476684a09b76444a43f4830909f18f776fa
Choddol <choddol@jojin-yeongs-MacBook-Pro.local>
2019-04-15 21:01:22 +0900

    done #1.39

--- src/utils/sendSMS.ts
+++ src/utils/sendSMS.ts
@@ -1,9 +1,5 @@
 import Twilio from "twilio";
 
-// const TWILIO_ACCOUNT_SID = "AC7cdc74b135411a9840739d9107fe7715";
-// const TWILIO_PHONE = "+12403770967";
-// const TWILIO_TOKEN = "17d66a6ccdab53d60252df5c6b823290";
-
 const TwilioClient = Twilio(
   process.env.TWILIO_ACCOUNT_SID,
   process.env.TWILIO_TOKEN
